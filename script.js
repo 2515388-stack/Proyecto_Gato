7fee4ae7c86d84d0d112a03004ec7814c70b382f
2515388-stack <2515388@cesba-queretaro.edu.mx>
2026-03-24 10:08:59 -0600

    Actualizado

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let isplayerOne = true;
 let cells = document.getElementsByClassName("cell");
 
-//HOLA
+// NUEVO: Variables para llevar la cuenta del marcador
+let scoreX = 0;
+let scoreO = 0;
 
-// NUEVO: Variable para saber si el juego ya terminó
+// Variable para saber si el juego ya terminó
 let isGameOver = false; 
 
 for (let i=0; i< cells.length; i++){
@@ -11,8 +13,7 @@ for (let i=0; i< cells.length; i++){
 }
 
 function userMove(e){
-
-    // NUEVO: Si el juego ya terminó, salimos de la función sin hacer nada
+    // Si el juego ya terminó, salimos de la función sin hacer nada
     if (isGameOver) {
         return; 
     }
@@ -43,12 +44,22 @@ function checkLine(c1, c2, c3){
     }
 }
 
+// CORREGIDO Y ACTUALIZADO: Función showWinner
 function showWinner(player){
-    document.querySelector('#results').innerHTML = player + " win";
-}{
-    
-    // NUEVO: Bloqueamos el tablero porque ya hay un ganador
+    // 1. Bloqueamos el tablero porque ya hay un ganador
     isGameOver = true; 
+    
+    // 2. Mostramos el mensaje de victoria
+    document.querySelector('#results').innerHTML = player + " gana!";
+
+    // 3. NUEVO: Sumamos el punto al marcador correspondiente
+    if (player === 'X') {
+        scoreX++;
+        document.getElementById('scoreX').innerText = scoreX;
+    } else if (player === 'O') {
+        scoreO++;
+        document.getElementById('scoreO').innerText = scoreO;
+    }
 }
 
 let restartBtn = document.getElementById("restart");
@@ -68,8 +79,6 @@ function restartGame() {
     // 3. Reiniciar el turno para que las 'X' empiecen de nuevo
     isplayerOne = true;
 
-    // NUEVO: Desbloquear el tablero para la nueva partida
+    // 4. Desbloquear el tablero para la nueva partida
     isGameOver = false; 
 }
-
-// Fin del código
